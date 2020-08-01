bffc76b933ee9b45c8ab8ced13ca6974c512840e
druloloy <dru.loloy@gmail.com>
2020-08-01 16:12:36 +0800

    WORK IN PROGRESS: NAVIGATION BAR

--- js/mainscript.js
+++ js/mainscript.js
@@ -3,36 +3,60 @@ var mobile_nav = $("#mobile-nav");
 var close = $("#close");
 
 var bgheader = $("#welcome-background");
+var tooltip = document.querySelectorAll(".project-tooltip")
+
+// pages
+var default_page = $("#welcome-section");
+
+//links
+var whoami_link = $("#link-whoami");
+var projects_link = $("#link-projects");
+var nav_links = document.querySelectorAll(".links-mobile");
 
 $(document).ready(function(){
-
 
     bgheader.css({
         "opacity":"1",
         "background-position":"right"
     });
 
+    document.addEventListener('mousemove',toolTip, false);
+
+    function toolTip(e){
+        for(var i = tooltip.length;i--;){
+            tooltip[i].style.left = e.pageX + 'px';
+            tooltip[i].style.top = e.pageY + 'px';
+        }
+    }
 
 
-
-
-
-
-
-
-    burger.click(function(){
+    function openNav(){
         burger.hide();
         close.show();
         mobile_nav.css("height","100vh");
-    });
-
-    close.click(function(){
+    }
+    function closeNav(){
         burger.show();
         close.hide();
         mobile_nav.css({
             "transition":"300ms ease-in-out",
             "height":"0"
         });
+    }
+    burger.click(openNav);
+    close.click(closeNav);
+
+    function switchPage(page_link){
+        var new_page = page_link.getAttribute("href");
+        console.log(new_page);
+    }
+    
+    nav_links[1].click(function(){
+        console.log("hello");
     });
+
+    for(var i = nav_links.length; i--;){
+        
+    }
 });
 
