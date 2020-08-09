529e7ebab3050ebb23165882f33dd257901b8b88
druloloy <dru.loloy@gmail.com>
2020-08-09 10:21:15 +0800

    Updated. Added Contact Section

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,3 +1,4 @@
+
 var burger = $("#burger-nav");
 var mobile_nav = $("#mobile-nav");
 var close = $("#close");
@@ -15,13 +16,11 @@ var contacts_link = $("#link-contacts");
 var nav_links = document.querySelector(".links-mobile");
 
 $(document).ready(function(){
-
-    alert("Test: Project Section ERROR_IN_PROJECT_TILES");
-    bgheader.css({
-        "opacity":"1",
-        "background-position":"right"
-    });
-
+        bgheader.css({
+            "opacity":"1",
+            "background-position":"right"
+        });
+   
     function openNav(){
         burger.hide();
         close.show();
@@ -37,46 +36,5 @@ $(document).ready(function(){
     }
     burger.click(openNav);
     close.click(closeNav);
-
-    function switchPage(){
-        var current_page, new_page,curr;
-
-        $(".links-mobile").on('click', function(){
-            var attr = this.id();
-            console.log(attr);
-            switch(attr){
-                case 'link-whoami':
-                    main_page.addClass("active-page");
-                    whoami_link.addClass("active-link");
-                    
-
-                    projects_link.removeClass("active-link");
-                    project_page.removeClass("active-page");
-                    contacts_link.removeClass("active-link");
-                    contacts_page.removeClass("active-page");
-
-                    closeNav();
-                    break;
-                case 'link-projects':
-                    project_page.addClass("active-page");
-                    project_link.addClass("active-link");
-                    
-
-                    whoami_link.removeClass("active-link");
-                    main_page.removeClass("active-page");
-                    contacts_link.removeClass("active-link");
-                    contacts_page.removeClass("active-page");
-
-                    closeNav();
-                    break;
-            }
-        });
-
-
-    function disablePages(exempt){
-
-    }
-        
-    }
+    
 });
-
